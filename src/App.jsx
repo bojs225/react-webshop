1168b9672a3c266aa148cdb34ca0280c32f81a79
bojs225 <bojanbrkic11@gmail.com>
2024-03-11 19:35:26 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <Header></Header>
+      <MainComponent></MainComponent>
+      <FooterComponent></FooterComponent>
+    </>
+  )
+}
+
+export default App
